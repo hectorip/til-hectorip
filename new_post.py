0c93939da2e6daf94c0d980dda38dbff428f3f3a
Héctor Iván Patricio Moreno <hectorivanpatriciomoreno@gmail.com>
2018-12-27 21:36:19 -0600

    new post script

---
+++ new_post.py
@@ -0,0 +1,24 @@
+import begin
+from datetime import datetime
+from slugify import slugify
+
+template = """---
+title: "{}"
+date: {}
+author: Héctor Patricio
+tags:
+comments: true
+excerpt: ""
+header:
+  image: http: // res.cloudinary.com / hectorip / image / upload / v1535950811 / residual_sixste.png
+---"""
+
+@begin.start(auto_convert=True)
+def main(*names):
+    """ Creates a new file with today's date and title as slug in _posts dir """
+    t = datetime.today()
+    date = "{}-{}-{}".format(t.year, t.month,t.day)
+    for name in names:
+        file_name = "_posts/{}-{}.md".format(date, slugify(name))
+        with open(file_name, "w") as f:
+            f.write(template.format(name, date))
